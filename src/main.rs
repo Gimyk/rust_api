3951b4c66f01721110bfdfa10cf6e7f8638459ff
gKafuwa <gkafuwa@gmail.com>
2019-06-02 17:46:31 +0200

    Auth Added just need to make sure that the token is auto generated not the same every time. this can be done by changing the secrete key(Auth key)

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ extern crate hyper;
 extern crate crypto;
 
 // for Nickel
-use nickel::{Nickel, HttpRouter, JsonBody, MediaType, Router};
+use nickel::{Nickel, HttpRouter, JsonBody, MediaType, Router, MiddlewareResult, Request, Response,};
 use nickel::status::StatusCode::{self, Forbidden};
 
 // for json parsin
@@ -28,7 +28,15 @@ use mongodb::error::Result as MongoResult;
 use bson::{Bson, Document};
 use bson::oid::ObjectId;
 
+// for auth
+use hyper::header;
+use hyper::header::{Authorization, Bearer};
 
+
+// jwt
+use std::default::Default;
+use crypto::sha2::Sha256;
+use jwt::{Header, Registered, Token};
 
 #[derive(Serialize, Deserialize)]
 pub struct User{
@@ -45,6 +53,13 @@ struct Atlass{
     continent: String
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct LoginStuff{
+    email: String,
+    password: String
+}
+
+static AUTH_SECRET: &'static str = "this should be my key";
 
 fn main() {
     let mut server = Nickel::new();
@@ -61,12 +76,89 @@ fn main() {
     // let data_b = conn_client.db("rust-users");
     // let data_b = conn_client.db("rust-users");
     
+    fn auth<'mw>(request: &mut Request, response: Response<'mw>, ) -> MiddlewareResult<'mw>{
+        if request.origin.method.to_string() == "OPTION".to_string(){
+           return response.next_middleware();
+        }else{
+            // We do not want to apply the middleware to the login route
+            if request.origin.uri.to_string() == "/login".to_string() {
+
+                return response.next_middleware();
+
+            }else{
+                 // Get the full Authorization header from the incoming request headers
+        let auth_header = match request.origin.headers.get::<Authorization<Bearer>>() {
+            Some(header) => header,
+            None => panic!("No authorization header found")
+        };
+
+        // Format the header to only take the value
+        let jwt = header::HeaderFormatter(auth_header).to_string();
+
+        // We don't need the Bearer part,
+        // so get whatever is after an index of 7
+        let jwt_slice = &jwt[7..];
+
+        // Parse the token
+        let token = Token::<Header, Registered>::parse(jwt_slice).unwrap();
+
+        // Get the secret key as bytes
+        let secret = AUTH_SECRET.as_bytes();
+
+        // Verify the token
+        if token.verify(&secret, Sha256::new()) {
+
+            return response.next_middleware();
+
+        } else {
+
+            return response.error(Forbidden, "Access denied");
+
+        }
+            }
+    }
+    }
+
     fn get_data_string(result: MongoResult<Document>) -> Result<Bson, String> {
         match result {
             Ok(doc) => Ok(Bson::Document(doc)),
             Err(e) => Err(format!("{}", e))
         }
     }
+
+    router.post("/login", middleware! { |request|
+
+        // Accept a JSON string that corresponds to the User struct
+        let log_user = request.json_as::<LoginStuff>().unwrap();
+
+        // Get the email and password
+        let email = log_user.email.to_string();
+        let password = log_user.password.to_string();
+
+        // Simple password checker
+        if password == "secret".to_string() {
+
+            let header: Header = Default::default();
+
+            // For the example, we just have one claim
+            // You would also want iss, exp, iat etc
+            let claims = Registered {
+                sub: Some(email.into()),
+                ..Default::default()
+            };
+
+            let token = Token::new(header, claims);
+
+            // Sign the token
+            let jwt = token.signed(AUTH_SECRET.as_bytes(), Sha256::new()).unwrap();
+
+            format!("this is my secrete key => {}", jwt)
+
+        } else {
+            format!("Incorrect username or password")
+        }
+
+    });
 
     router.get("/users", middleware! {|_request, mut _res|
         println!("Gettingt he data");
@@ -109,7 +201,6 @@ fn main() {
     
     
     });
-
 
     router.post("/users/new", middleware! {|_req, _res|
         println!("Posting the data");
@@ -308,6 +399,7 @@ fn main() {
         }
     });
 
+    server.utilize(auth);
     server.utilize(router);
     server.utilize(count);
     server.listen("127.0.0.1:9000");
